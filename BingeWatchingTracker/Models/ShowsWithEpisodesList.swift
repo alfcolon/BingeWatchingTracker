deea82a6df1c86a25562eef034d4b0a43a1ac4e2
alfcolon <alfcolon@gmail.com>
2020-01-02 19:22:01 -0800

    Pushing a draft of the group project I've been working on for teammate to see. Naming conventions could use some work.

---
+++ BingeWatchingTracker/Models/ShowsWithEpisodesList.swift
@@ -0,0 +1,49 @@
+//
+//  ShowsWithEpisodesList.swift
+//  BingeWatchingTracker
+//
+//  Created by alfredo on 12/24/19.
+//  Copyright © 2019 TeamFlash. All rights reserved.
+//
+
+import Foundation
+
+class BingeController {
+    
+    var allShows: [Show] =
+            [
+                Show(name: "Arrow", imageName: "",
+                     episodes: [
+                        Episode(name: "Pilot", binged: false),
+                        Episode(name: "Honor Thy Father", binged: false),
+                        Episode(name: "Lone Gunmen", binged: false),
+                        Episode(name: "An Innocent Man", binged: false),
+                        Episode(name: "Damaged", binged: false),
+                        Episode(name: "Legacies", binged: false),
+                        Episode(name: "Muse Of Fire", binged: false)
+                    ],
+                    favorite: false),
+                Show(name: "The Flash", imageName: "",
+                     episodes: [
+                    Episode(name: "Pilot", binged: false),
+                    Episode(name: "Fastest Man Alive", binged: false),
+                    Episode(name: "Things You Cannout Outrun", binged: false),
+                    Episode(name: "Going Rogue", binged: false),
+                    Episode(name: "Plastique", binged: false),
+                    Episode(name: "The Flash Is Born", binged: false),
+                    Episode(name: "Power Outage", binged: false)
+                ],
+                favorite: false),
+                Show(name: "DareDevil", imageName: "",
+                     episodes: [
+                    Episode(name: "Into The Ring", binged: false),
+                    Episode(name: "Cut Man", binged: false),
+                    Episode(name: "Rabbit In A Snowstorm", binged: false),
+                    Episode(name: "In The Blood", binged: false),
+                    Episode(name: "World On Fire", binged: false),
+                    Episode(name: "Condemned", binged: false),
+                    Episode(name: "Stick", binged: false)
+            ],
+            favorite: false)
+        ]
+}
---
+++ BingeWatchingTracker/Views/ShowTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  ShowTableViewCell.swift
+//  BingeWatchingTracker
+//
+//  Created by alfredo on 12/24/19.
+//  Copyright © 2019 TeamFlash. All rights reserved.
+//
+
+import UIKit
+
+class ShowTableViewCell: UITableViewCell{
+    
+    //MARK: -Outlets
+    
+    @IBOutlet weak var showLabel: UILabel!
+    
+    //MARK: -Properties
+    
+    var show: Show?{
+        didSet{
+            updateViews()
+        }
+    }
+    var row: Int?
+    var delegate: UpdateShowInformation?
+    
+    //MARK: -Methods
+    
+    private func updateViews(){
+        guard let show = show else { return }
+        showLabel.text = show.name
+    }
+    @IBAction func favoriteTapped(_ sender: UIButton) {
+        guard let _ = row else { return }
+        delegate?.informationToUpdate(showIndex: row!, episodeIndex: nil, favorited: !show!.favorite)
+    }
+}
